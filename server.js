2222b12591c1abdd536b390ed10c853cdf95c453
ronarceo <ronaldmarceo@gmail.com>
2021-08-26 20:38:39 -0500

    added start prompt with switch statement
    functions aside from start and quit not yet implemented
    installed npm packages

--- server.js
+++ server.js
@@ -14,3 +14,78 @@ const db = mysql.createConnection(
     },
     console.log(`Connected to the employees_db database.`)
 );
+
+db.connect(function(err) {
+    if (err) throw err;
+    start();
+});
+
+function start() {
+    inquirer.prompt({
+        type: "list",
+        choices: ["View departments", "View roles", "View employees", "Add department", "Add role", "Add employee",  "Update employee role", "Quit"],
+        message: "What would you like to do?",
+        name: "startChoice"
+    })
+        .then(response => {
+            console.log(response.startChoice);
+
+            switch (response.startChoice) {
+                case "View departments":
+                    viewDepartments();
+                    break;
+                case "View roles":
+                    viewRoles();
+                    break;
+                case "View employees":
+                    viewEmployees();
+                    break;
+                case "Add department":
+                    addDepartment();
+                    break;
+                case "Add role":
+                    addRole();
+                    break;
+                case "Add employee":
+                    addEmployee();
+                    break;
+                case "Update employee role":
+                    updateEmployee();
+                    break;
+                default:
+                    quit();
+            }
+        })
+}
+
+function viewDepartments() {
+    start();
+}
+
+function viewRoles() {
+    start();
+}
+
+function viewEmployees() {
+    start();
+}
+
+function addDepartment() {
+    start();
+}
+
+function addRole() {
+    start();
+}
+
+function addEmployee() {
+    start();
+}
+
+function updateEmployee() {
+    start();
+}
+
+function quit() {
+    process.exit();
+}
